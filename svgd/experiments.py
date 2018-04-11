7cb3a2a317bb329160334cebdd4d62f955e70b11
minasyan <minasyan@mit.edu>
2018-04-11 12:18:38 -0400

    modified structure, run experiments easier

--- svgd/experiments.py
+++ svgd/experiments.py
@@ -3,16 +3,26 @@ import numpy.matlib as nm
 import svgd
 import torch
 from torch.distributions import Normal
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde, norm
 
 
-def OneDimNormal(x):
+def OneDimNormalMixture(x):
     return (1/3) * torch.exp(Normal(-2,1).log_prob(x)) + (2/3) * torch.exp(Normal(2,1).log_prob(x))
 
+def SimpleTwoDim(x):
+    mean = torch.Tensor([0, 0])
+    covariance = torch.Tensor([[1, -10], [-10, 1]])
+    #return torch.exp()
 
-x = np.random.normal(-10, 1, 100).reshape((-1, 1))
-h = 1
 
-result = svgd.svgd(OneDimNormal, svgd.RBF(1), x, 1000)
-plt.hist(result.data.numpy(), bins=20)
-plt.show()
+x = np.random.normal(-10, 1, 200).reshape((-1, 2))
+
+result = svgd.svgd(OneDimNormalMixture, svgd.RBF_kernel, x, 500)
+# g = gaussian_kde(result.numpy().reshape(-1))
+# xs = np.arange(-10, 10, 0.01)
+# plt.plot(xs, svgd.numpy_p(xs), 'r-')
+# plt.plot(xs, g(xs), 'g')
+# plt.show()
+#plt.hist(result.data.numpy(), bins=20)
+#plt.show()
--- svgd/svgd.py
+++ svgd/svgd.py
@@ -2,6 +2,11 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde, norm
+
+# ## debugging
+# def numpy_p(x):
+#     return 1/3 * norm.pdf(x, -2, 1) + 2/3 * norm.pdf(x, 2, 1)
 
 '''
 Runs T number of iterations of Stein Variational
@@ -19,66 +24,92 @@ Input: p - target density
 
 Output: final set of points after T iterations.
 '''
-def svgd(p, k, x, T, alpha=0.9, fudge=1e-6, step=1e-1):
-    assert len(x.shape) == 2 and x.shape[1] == 1
-    n = x.shape[0]
-    x = Variable(torch.Tensor(x), requires_grad = True)
-    accumulated_grad = torch.zeros((n, 1))
-    #import pdb; pdb.set_trace()
+def svgd(p, kern, x, T, alpha=0.9, fudge=1e-6, step=1e-1):
+    assert len(x.shape) == 2
+    n, d = x.shape
+    x = torch.Tensor(x)
+    accumulated_grad = torch.zeros((n, d))
 
     for i in range(T):
-        if i % 20 == 0:
-            print(i)
-            print(torch.mean(x).data)
-            plt.hist(x.data.numpy(), bins=20)
-            plt.show()
-        grad_logp = grad_log(p, x)
-        kernel, grad_kernel = k_matrix(k, x)
-        phi = torch.matmul(kernel, grad_logp)/n + torch.mean(grad_kernel, dim=1).view(-1, 1)
+        ### debugging
+        # print(i)
+        # print(torch.mean(x))
+        # if i == 0 or i == 50 or i == 75 or i == 100 or i == 150:
+        #     xs = np.arange(-10, 10, 0.01)
+        #     plt.plot(xs, numpy_p(xs), 'r-')
+        #     g = gaussian_kde(x.numpy().reshape(-1))
+        #     plt.plot(xs, g(xs), 'g')
+        #     plt.show()
+
+        varx = Variable(x, requires_grad = True)
+        grad_logp = grad_log(p, varx)
+        kernel, grad_kernel = kern(x)
+        phi = torch.matmul(kernel, grad_logp)/n + torch.mean(grad_kernel, dim=1).view(n, d)
         if i == 0:
             accumulated_grad += phi**2
         else:
             accumulated_grad = alpha * accumulated_grad + (1-alpha) * phi**2
         stepsize = fudge + torch.sqrt(accumulated_grad)
-        newx = x.data + torch.div(step * phi, stepsize)
-        x = Variable(newx, requires_grad=True)
+        x = x + torch.div(step * phi, stepsize)
     return x
 
+'''
+Build the RBF kernel matrix and grad_kernel matrix
+given a set of points x.
 
+Input: n x d set of points - x
+Output: n x n kernel matrix, n x n x d grad_kernel matrix
+'''
+def RBF_kernel(x):
+    n, d = tuple(x.size())
+    pairdist = torch.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            pairdist[i][j] = torch.dist(x[i], x[j])
+    med = torch.median(pairdist)
+    h = med**2 / np.log(n)
+
+    kernel = torch.exp(-(1/h) * pairdist**2)
+    grad_kernel = torch.zeros((n, n, d))
+    for i in range(n):
+        for j in range(n):
+            grad_kernel[i][j] = 2/h * kernel[i][j] * (x[i]-x[j])
+    return torch.Tensor(kernel), torch.Tensor(grad_kernel)
 
 '''
 Returns RBF kernel.
 
-Input: bandwith if the kernel - h
+Input: bandwith of the kernel - h
 Output: a function that returns the RBF kernel
 '''
 def RBF(h):
     def kernel(x1,x2):
-        return torch.exp(-(1/h) * torch.norm(x1 - x2))
+        return np.exp(-(1/h) * torch.norm(x1 - x2)**2)
     return kernel
 
 '''
-Returns gradient of the RBF kernel
+Returns the gradient of the RBF kernel w.r.t. x2
 '''
 def RBF_grad(x1,x2):
-    return (2) * (x1 - x2) * torch.exp(-torch.norm(x1 -x2)**2)
+    return (2) * (x1 - x2) * np.exp(-torch.norm(x1 -x2)**2)
 
 '''
 Takes the gradient of the log of the unnormalized
 probability distribution.
 
 Input: p - unnormalized probability distribution
-       x - set of current particules
+       x - set of current particles
 
 Output: gradient of log of distribution p at point x
 '''
 def grad_log(p, x):
+    n, d = tuple(x.size())
     grad_log = []
     for i in range(len(x)):
         logp = torch.log(p(x[i]))
         logp.backward()
         grad_log.append(x.grad.data[i])
-    grad_log = np.array(grad_log).reshape((-1, 1))
+    grad_log = np.array(grad_log).reshape((n, d))
     return torch.Tensor(grad_log)
 
 
@@ -90,7 +121,7 @@ Input: k - kernel function
 
 Output: matrix containing the kernel evaluated at each pair of points
 '''
-def k_matrix(k, x):
+def k_matrix(k, k_grad, x):
     #import pdb; pdb.set_trace()
     n = len(x)
     kernel_matrix, grad_kernel_matrix = [], []
@@ -99,9 +130,9 @@ def k_matrix(k, x):
         grad_kernel_matrix.append([])
         for j in range(n):
             kernel_val = k(x[i], x[j])
-            kernel_matrix[-1].append(float(kernel_val.data))
+            kernel_matrix[-1].append(float(kernel_val))
             #kernel_val.backward()
-            kernel_grad = RBF_grad(x[i], x[j])
-            grad_kernel_matrix[-1].append(float(kernel_grad.data))
+            kernel_grad = k_grad(x[i], x[j])
+            grad_kernel_matrix[-1].append(float(kernel_grad))
             #x.grad.data.zero_()
     return torch.Tensor(kernel_matrix), torch.Tensor(grad_kernel_matrix)
